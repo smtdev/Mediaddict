f5813b7cd2d35a54377df9cfc10388d3624634e9
Sergio Martín <sergio.martin.te@gmail.com>
2020-10-13 02:36:05 +0200

    gradle update

--- app/src/main/java/me/sergiomartin/tvshowmovietracker/MainActivity.java
+++ app/src/main/java/me/sergiomartin/tvshowmovietracker/MainActivity.java
@@ -1,13 +1,22 @@
 package me.sergiomartin.tvshowmovietracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import me.sergiomartin.tvshowmovietracker.databinding.ActivityMainBinding;
+
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(view -> Toast.makeText(this, "Test", Toast.LENGTH_LONG).show());
     }
+
 }
